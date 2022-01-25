6feb790a13622fb630d85f55d0be93a35a74e2bd
Gustavo Pedro Lima <gpedr56@gmail.com>
2022-01-25 14:49:25 -0300

    .

--- Sistema de Usuario/index.js
+++ Sistema de Usuario/index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //View Engine
 
 app.set('view engine', 'ejs');
 
+//Estatics
 app.use(express.static('public'));
 
-app.get('/',(req, res) => {
+
+//Rot
+app.get('/', (req, res) => {
     res.render("index");
 });
 
+app.post('/cadastrar', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    
+});
 
-app.listen('8080',()=>{console.log("App rodando!")})
+app.listen('8080', () => { console.log("App rodando!") })
