cee2df9dcc3908e9c3b82bb812be0e3fe8215e77
Gustavo Pedro Lima <gpedr56@gmail.com>
2022-01-26 17:31:29 -0300

    ..

--- Sistema de Usuario/index.js
+++ Sistema de Usuario/index.js
@@ -4,8 +4,16 @@ const bodyParser = require('body-parser');
 const connection = require("./database/database");
 const User = require('./database/User');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+
+//Sessions
+
+app.use(session({
+    secret: 'oaksdopaksdpoaskdapsokd', cookie: { maxAge: 60000}
+}))
 
 
+//database
 connection.authenticate()
     .then(()=>console.log('Sucesso'))
     .catch((error)=>console.log(error))
@@ -26,19 +34,74 @@ app.get('/', (req, res) => {
     res.render("index");
 });
 
-app.post('/cadastrar', (req, res) => {
+
+app.get('/cadastrar',(req,res)=>{
+    res.render("cadastrar");
+});
+
+app.post('/cadastrarNovo', (req, res) => {
     let salt = bcrypt.genSaltSync(10)
     let email = req.body.email;
     let password = bcrypt.hashSync(req.body.password, salt);
     
-
-    User.create({
-        email:email,
-        password:password
-    }).then(()=>{
-        res.redirect('/');
+    User.findOne({where:{email:email}}).then(user=>{
+        if(user == undefined){
+            User.create({
+                email:email,
+                password:password
+            }).then(()=>{
+                res.redirect('/');
+            })
+        }else{
+            res.redirect('/')
+        }
     })
 
+});
+
+app.post('/login',(req,res)=>{
+    let email = req.body.email;
+    let password = req.body.password;
+
+
+    User.findOne({where:{email:email}}).then(user=>{
+        if(user !=undefined){
+            let correct = bcrypt.compareSync(password,user.password);
+
+            if(correct){
+                req.session.user = {
+                    email: user.email
+                }
+                res.redirect('/listagem')
+            }else{
+                res.redirect('/')
+            }
+        }else{
+            res.redirect('/')
+        }
+    })
+});
+
+app.get('/listagem',(req,res)=>{
+    User.findAll({raw:true}).then(usuarios=>{res.render('listagem',{usuarios})});
+});
+
+app.post('/user/delete',(req,res)=>{
+    let id = req.body.id;
+
+    User.findOne({where:{id:id}}).then(user=>{
+        if(user == undefined){
+            res.redirect('/listagem');
+        }else{
+            User.destroy({
+                where:{
+                    id:id
+                }
+            }).then(()=>{
+                res.redirect('/listagem');
+            })
+        }
+    })
 });
 
 app.listen('8080', () => { console.log("App rodando!") })
